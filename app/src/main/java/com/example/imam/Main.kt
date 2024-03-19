0ce462417a240893ec5b84f8661ef58777e895c0
Yoruu <159858517+ramadhanizx@users.noreply.github.com>
2024-03-19 13:49:38 +0700

    Add files via upload

---
+++ app/src/main/java/com/example/imam/Main.kt
@@ -0,0 +1,13 @@
+package com.example.imam
+import java.lang.Math.PI
+
+fun buildAquarium() {
+    val myAquarium = Aquarium(width = 25, length = 25, height = 40)
+    myAquarium.printSize()
+    val myTower = TowerTank(diameter = 25, height = 40)
+    myTower.printSize()
+}
+fun main() {
+    buildAquarium()
+
+}
